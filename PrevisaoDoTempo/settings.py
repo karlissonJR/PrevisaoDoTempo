8d6ed3c6d0ddda4babd901f36c0c066225acead0
karlissonJR <karlissonj@gmail.com>
2022-08-16 09:37:09 -0300

    configuração heroku

--- PrevisaoDoTempo/settings.py
+++ PrevisaoDoTempo/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 import environ
 import os
 
+import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -142,3 +144,5 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
